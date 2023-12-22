90e0da137b0dabc4d901815e0b3483f9b9573abe
Samir Khadka <47472827+samirkhadka998@users.noreply.github.com>
2023-12-22 07:52:55 +0545

    Gameboard, Coordinates class, hit/sunk/receive attack function implemented

--- index.js
+++ index.js
@@ -1,15 +1,64 @@
-class Ship{
-    constructor(length, hit, sunk){
+class Ship {
+    constructor(length, hit, sunk) {
         this.length = length;
         this.hit = hit;
         this.sunk = sunk;
     }
 }
 
-function hit(ship){
-    ship.hit++;
+class GameBoard {
+    constructor(ship, coordinates) {
+        this.ship = ship;
+        this.coordinates = coordinates;
+    }
+
+    placeShip() {
+        console.log("Ship is moved to " + this.coordinates);
+    }
 }
 
-function isSunk(ship){
+class Coordinates {
+    constructor(row, column, used = false) {
+        this.row = row;
+        this.column = column;
+        this.used = used;
+
+    }
+
+    changeCoordinateStatus(coordinates, used = true) {
+        coordinates.used = used;
+    }
+}
+let ship1 = new Ship(3, 0, false);
+let gameBoard1 = new GameBoard(ship1, new Coordinates(0, 0));
+
+let ship2 = new Ship(3, 0, false);
+let gameBoard2 = new GameBoard(ship1, new Coordinates(5, 0));
+
+let gameBoards = [gameBoard1, gameBoard2];
+
+
+
+function hit(ship) {
+    if (ship.hit != ship.length) {
+        ship.hit++;
+    }
+}
+
+function isSunk(ship) {
     return ship.hit === ship.length;
+}
+
+function receiveAttack(coordinates) {
+        let gameboard = gameBoards.filter(o => o.coordinates = coordinates && !o.coordinates.isused)
+        if (gameboard) {
+            hit(gameBoard.ship);
+
+            let gameover = gameBoards.every(o => isSunk(o.ship));
+            alert('gameover');
+        }
+
+
+    
+
 }
