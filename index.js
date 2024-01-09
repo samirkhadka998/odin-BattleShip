01e5d897302e88cb2b9bce20ba1de68565acb6a6
Samir Khadka <47472827+samirkhadka998@users.noreply.github.com>
2024-01-09 07:26:22 +0545

    Added option for manually adding ship or creating dummy data for user gameboard

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ while (end < 101) {
 start = 1, end = 91;
 while (end < 101) {
     let combo = [];
-    for (let index = start; index <= end; index+=10) {
+    for (let index = start; index <= end; index += 10) {
         combo.push(index);
     }
     combos.push(combo);
@@ -62,10 +62,10 @@ console.log(combos);
 
 
 let humanGameBoards = [];
-for(let i = 1 ; i <= 100 ; i++){
+for (let i = 1; i <= 100; i++) {
     let coordinate = new Coordinates(i, false);
-    humanGameBoards.push(new GameBoard(null,coordinate));
-    
+    humanGameBoards.push(new GameBoard(null, coordinate));
+
 }
 createHumanBoard(humanGameBoards);
 
@@ -102,10 +102,94 @@ function receiveAttack(num) {
 function ComputerMove(gameBoard) {
     let com = new Player("Computer");
     receiveAttack(gameBoard.coordinates, com)
-    
+
 }
 
 //Dom
+let rangeBtn = document.querySelector('#rangeBtn');
+rangeBtn.addEventListener('click', checkValidInput);
+let boardClick = false;
+
+dummyInsert();
+
+function dummyInsert(){
+    for(let i = 1 ; i < 16; i++){
+        if(i == 10) continue;
+        let div = document.querySelector(`.h${i}`)
+        div.classList.add('blue');
+    }
+
+    let rangeDiv = document.querySelector('#range');
+                rangeDiv.hidden = true;
+                boardClick = true;
+}
+
+let range = [0, 1, 2, 3, 4]
+
+function checkValidInput() {
+    let inputStart = document.querySelector('.start');
+    let inputEnd = document.querySelector('.end');
+
+    if (!inputStart.value || !inputEnd.value) {
+        alert('Invalid Input');
+        return;
+    }
+    let start = inputStart.value * 1;
+    let end = inputEnd.value * 1;
+    for (const combo of combos) {
+        if (combo.includes(start) && combo.includes(end)) {
+            let indexStart = combo.indexOf(start);
+            let indexEnd = combo.indexOf(end);
+            if (indexEnd - indexStart !== range[range.length - 1]) {
+                alert('Invalid Input');
+                return;
+            }
+            let gameBoards = [];
+            for (let index = indexStart; index <= indexEnd; index++) {
+                let num = combo[index];
+                let gameBoard = humanGameBoards.find(o => o.ship == null && o.coordinates.num == num);
+                if (!gameBoard) {
+                    alert('Invalid Input');
+                    return;
+
+                }
+                gameBoards.push(gameBoard);
+
+            }
+
+            Array.from(gameBoards).forEach(g => {
+                let div = document.querySelector(`.h${g.coordinates.num}`)
+                div.classList.add('blue');
+                let length = range[range.length - 1] + 1;
+                let ship = new Ship(length, 0, 0);
+                g.ship = ship;
+                g.coordinates.isused = true;
+            })
+
+            range.pop();
+
+            if (range.length == 1) {
+                let rangeDiv = document.querySelector('#range');
+                rangeDiv.hidden = true;
+                boardClick = true;
+
+            }
+            else {
+                let rangePara = document.querySelector('.rangePara');
+                rangePara.textContent = `Enter ${range.length} range`
+
+            }
+
+            return;
+
+
+        }
+    }
+    alert('Invalid Input');
+    return;
+
+}
+
 function createHumanBoard(humanGameBoards) {
     let humanBoard = document.querySelector('#humanBoard');
     humanGameBoards.forEach(element => {
@@ -119,18 +203,20 @@ function createHumanBoard(humanGameBoards) {
     let childrens = humanBoard.querySelectorAll('div');
 
     Array.from(childrens).forEach(c => {
-        c.addEventListener('click', attackOnOpposition )
+        c.addEventListener('click', attackOnOpposition)
     })
 
 }
 
 function attackOnOpposition(e) {
-    if(!e.target.className.includes('hit')){
+    if (!e.target.className.includes('hit') && boardClick) {
         let num = e.target.className.substring(1);
         e.target.classList.add('hit');
         receiveAttack(num);
     }
-   
+
 }
+
+
 
 
